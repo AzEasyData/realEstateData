e875ff67aac847d61331fc9e81b29316ea2af8ac
AzEasyData <139927598+AzEasyData@users.noreply.github.com>
2023-07-28 16:52:23 -0400

    Update production_2_staging.sql

--- dbt_transformations/models/production_2_staging.sql
+++ dbt_transformations/models/production_2_staging.sql
@@ -1,21 +1,61 @@
-
-
 -- Insert the records from the source table to the destination table
-INSERT INTO "HOUSING_MARKET_PRODUCTION"."PUBLIC"."Property_Sale_Listings" (
+
+INSERT INTO HOUSING_MARKET_PRODUCTION.PUBLIC.Property_Sale_Listings (
   -- Specify the columns to insert data into
-  -- Adjust the column names as per your requirements
-  id,
-  city
-  -- Add more columns as needed
+  ADDRESS_LINE_1,
+  BATHROOMS,
+  BEDROOMS,
+  CITY,
+  COUNTY,
+  CREATED_DATE,
+  DAYS_ON_MARKET,
+  FORMATTED_ADDRESS,
+  ID,
+  LAST_SEEN,
+  LATITUDE,
+  LISTED_DATE,
+  LONGITUDE,
+  LOT_SIZE,
+  OBJECTID,
+  PRICE,
+  PROPERTY_TYPE,
+  REMOVED_DATE,
+  SQUARE_FOOTAGE,
+  STATE,
+  STATUS,
+  YEAR_BUILT,
+  ZIP_CODE
+
 )
 SELECT
-  -- Select the columns from the source table
-  -- Adjust the column names as per your requirements
-  id,
-  city
-  -- Add more columns as needed
-FROM "HOUSING_MARKET_STAGING"."PUBLIC"."PROPERTY_SALE_LISTINGS"
-WHERE city IN (
+  -- Select the columns from the source table 
+  ADDRESS_LINE_1,
+  BATHROOMS,
+  BEDROOMS,
+  CITY,
+  COUNTY,
+  CREATED_DATE,
+  DAYS_ON_MARKET,
+  FORMATTED_ADDRESS,
+  ID,
+  LAST_SEEN,
+  LATITUDE,
+  LISTED_DATE,
+  LONGITUDE,
+  LOT_SIZE,
+  OBJECTID,
+  PRICE,
+  PROPERTY_TYPE,
+  REMOVED_DATE,
+  SQUARE_FOOTAGE,
+  STATE,
+  STATUS,
+  YEAR_BUILT,
+  ZIP_CODE
+
+FROM HOUSING_MARKET_STAGING.PUBLIC.PROPERTY_SALE_LISTINGS
+  
+WHERE CITY IN (
   'Silver Spring', 'Rockville', 'Bethesda', 'Hyattsville', 'Chevy Chase',
   'Takoma Park', 'College Park', 'Riverdale', 'Potomac', 'Brentwood',
   'Kensington', 'Mount Rainier', 'University Park', 'Glenn Dale',
